1363c6bc58278260187f3f9858b5a67485bb31a9
shinco0327 <sam@551.com.tw>
2020-12-19 17:05:13 +0800

    20201219_toMongo_serial

--- local/toMongo.py
+++ local/toMongo.py
@@ -1,7 +1,11 @@
 import pymongo
 import time
 from datetime import datetime
+import serial
 
+strPort='com3'
+ser = serial.Serial(strPort, 115200)
+ser.flush()
 
 conn = pymongo.MongoClient('mongodb://128.199.118.43:27017/')
 
@@ -10,9 +14,10 @@ db = conn['admin']
 real_time = db.real_time
 while 1:
     try:
-        real_time.insert_one({'value':512 ,'time':datetime.now()})
+        data = float(ser.readline())
+        print(data)
+        real_time.insert_one({'value':data ,'time':datetime.now()})
         db.device.update_many({},{'$set':{'alive': True}})
-        time.sleep(1)
     except Exception as e:
         if e == KeyboardInterrupt:
             break
