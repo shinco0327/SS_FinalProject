c280d80e8a6c3f0fb75c36899d32390820ebe0e7
shinco0327 <sam@551.com.tw>
2020-12-19 16:56:18 +0800

    20201219_toMongo_alive

--- local/toMongo.py
+++ local/toMongo.py
@@ -6,9 +6,17 @@ from datetime import datetime
 conn = pymongo.MongoClient('mongodb://128.199.118.43:27017/')
 
 
-database = conn['admin']
-real_time = database['real_time']
-real_time.insert_one({'value':512 ,'time':datetime.now()})
-time.sleep(1)
+db = conn['admin']
+real_time = db.real_time
+while 1:
+    try:
+        real_time.insert_one({'value':512 ,'time':datetime.now()})
+        db.device.update_many({},{'$set':{'alive': True}})
+        time.sleep(1)
+    except Exception as e:
+        if e == KeyboardInterrupt:
+            break
+        else:
+             pass
 
 print('done')
